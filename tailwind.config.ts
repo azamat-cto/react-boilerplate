ce7fa4f1616ed0e85fe522ea32ef219964b86f76
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-10-12 21:17:11 +0500

    Add shadcn accordion component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from "tailwindcss";
+import tailwindAnimate from "tailwindcss-animate";
 
 export default {
 	darkMode: ["class"],
-	content: ["./index.html", "./src/**/*.{js,ts,tsx}"],
+	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
 			borderRadius: {
@@ -52,7 +53,29 @@ export default {
 					"5": "hsl(var(--chart-5))",
 				},
 			},
+			keyframes: {
+				"accordion-down": {
+					from: {
+						height: "0",
+					},
+					to: {
+						height: "var(--radix-accordion-content-height)",
+					},
+				},
+				"accordion-up": {
+					from: {
+						height: "var(--radix-accordion-content-height)",
+					},
+					to: {
+						height: "0",
+					},
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
 		},
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindAnimate],
 } satisfies Config;
