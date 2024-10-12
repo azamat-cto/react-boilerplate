a3c54f29073528f51f0f12c61923a701eccd17f9
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-10-12 18:13:59 +0500

    Add biome

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 function bootstrap() {
-  const container = document.getElementById('root') as Element
+	const container = document.getElementById("root") as Element;
 
-  if (!container.innerHTML) {
-    const root = createRoot(container)
-    root.render(
-      <StrictMode>
-        <App />
-      </StrictMode>,
-    )
-  }
+	if (!container.innerHTML) {
+		const root = createRoot(container);
+		root.render(
+			<StrictMode>
+				<App />
+			</StrictMode>,
+		);
+	}
 }
 
-bootstrap()
+bootstrap();
