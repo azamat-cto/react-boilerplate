44ebc3c0f77c94d8e607fddfad8ceba1b9b1fb79
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-10-12 13:03:50 +0500

    Clean project

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function bootstrap() {
+  const container = document.getElementById('root') as Element
+
+  if (!container.innerHTML) {
+    const root = createRoot(container)
+    root.render(
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    )
+  }
+}
+
+bootstrap()
